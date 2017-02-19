4d1d39b96ef866b80a8386fc5268bc06eb5fac28
vitomedved <vito.medved12@gmail.com>
2017-02-19 18:10:24 +0100

    Adding description
    Added more detailed description, YT demo link, and Imgur image links of
    project.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Arduino_Piano
-Ardunio piano with 4 buttons, when a button is pressed LED lights up, and a tone is played.
+Ardunio piano with 4 buttons, when a button is pressed LED lights up, and a tone of certain frequency is played.
+Youtube demo: https://www.youtube.com/watch?v=0QT2gfAaVkQ
+Pictures of project on Imgur: http://imgur.com/a/2Y5tr
--- piano_project.ino
+++ piano_project.ino
@@ -1,9 +1,11 @@
-//initializing b - button
+//initializing b - buttons to arduino pins 2, 3, 4, 5
+//LEDs are connected between a button and a ground
 const int b1 = 2, b2 = 3, b3 = 4, b4 = 5;
 
 void setup(){
+  //starting serial print to console
   Serial.begin(9600);
-  //setting button as an input
+  //setting buttons and pin 8 (tone) as an inputs
   pinMode(b1, INPUT);
   pinMode(b2, INPUT);
   pinMode(b3, INPUT);
@@ -28,7 +30,7 @@ void loop(){
     tone(A0, 390, 20);
   }
   
-  //reading from b1 to see changes
+  //reading input state (0 or 1) for all buttons
   Serial.print(digitalRead(b4));
   Serial.print(digitalRead(b3));
   Serial.print(digitalRead(b2));
